3969f2340f64d98482795060009972487971b120
audy <veeaudy1201@gmail.com>
2025-06-21 00:56:18 -0400

    improved about section ui functionality

--- src/app/data.ts
+++ src/app/data.ts
@@ -7,6 +7,7 @@ type Project = {
   name: string
   description: string
   tags: ProjectTag[]
+  link: string
 }
 
 type TimelineItem = {
@@ -43,22 +44,44 @@ export const PROJECTS: Project[] = [
   {
     name: 'Rollcall',
     description: 'task-management mobile application that inspires productivity through focused features like streaks, motivation, and simple task tracking.',
+    link: 'https://github.com/nathanwince/Task-Manager',
     tags: [
       { label: 'Figma', color: 'bg-blue-500' },
       { label: 'Flutter', color: 'bg-sky-400' },
       { label: 'FastAPI', color: 'bg-gray-800' },
-      { label: 'MySQL', color: 'bg-green-600' },
+      { label: 'SQL', color: 'bg-indigo-600' },
       { label: 'Javascript', color: 'bg-yellow-500' },
     ],
   },
   {
     name: 'Joynest',
     description: 'e-commerce platform where users can buy, offer, and sell a wide variety of home decor and furniture',
+    link:'https://github.com/lancanon/e-commere-project--2024-',
     tags: [
       { label: 'JavaScript', color: 'bg-yellow-500' },
       { label: 'Node.js', color: 'bg-red-600' },
       { label: 'Express.js', color: 'bg-orange-500' },
       { label: 'MongoDB', color: 'bg-purple-400' },
+    ],
+  },
+  {
+    name: 'lannymon',
+    description: 'personal social links for lannymon.net (discontinued)',
+    link:'https://lancanon.github.io/',
+    tags: [
+      { label: 'HTML', color: 'bg-green-500' },
+      { label: 'CSS', color: 'bg-pink-600' },
+      { label: 'JavaScript', color: 'bg-yellow-500' },
+      { label: 'FastAPI', color: 'bg-gray-800' },
+      { label: 'AWS', color: 'bg-orange-600' },
+    ],
+  },
+  {
+    name: 'Furusato',
+    description: '2D platformer where the player guides a lost girl through a mysterious world to find her way back home.',
+    link:'https://github.com/lancanon/e-commere-project--2024-',
+    tags: [
+      { label: 'GameMaker', color: 'bg-lime-500' },
     ],
   },
 ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import Link from 'next/link'
 import {
   PROJECTS,
   TIMELINE,
-  EMAIL,
-  SOCIAL_LINKS,
 } from './data'
 
 const VARIANTS_CONTAINER = {
@@ -128,26 +126,30 @@ export default function Personal() {
         <h3 className="mb-5 text-lg font-bold">Projects</h3>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
           {PROJECTS.map((project) => (
-            <div key={project.name} className="space-y-2">
-              <div className="px-1">
-                <div className="font-base group relative inline-block font-[450] text-zinc-900 dark:text-zinc-50">
-                  {project.name}
-                </div>
-                <p className="text-base text-zinc-600 dark:text-zinc-400">
-                  {project.description}
-                </p>
-                <div className="flex flex-wrap gap-2 mt-2">
-                  {project.tags.map((tag) => (
-                    <span
-                      key={tag.label}
-                      className={`px-2 py-1 rounded-full text-xs font-medium text-white ${tag.color}`}
-                    >
-                      {tag.label}
-                    </span>
-                  ))}
-                </div>
+            <a
+              key={project.name}
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group block rounded-2xl bg-zinc-50/40 p-4 ring-1 ring-zinc-200/50 ring-inset shadow transition-all duration-200 hover:bg-white hover:shadow-xl dark:bg-zinc-900/40 dark:ring-zinc-800/50 dark:hover:bg-[#1e2936] transform hover:scale-105"
+            >
+              <div className="font-base relative inline-block font-[450] text-zinc-900 dark:text-zinc-50 group-hover:text-blue-600 transition-colors">
+                {project.name}
               </div>
-            </div>
+              <p className="mt-1 text-base text-zinc-600 dark:text-zinc-400">
+                {project.description}
+              </p>
+              <div className="flex flex-wrap gap-2 mt-2">
+                {project.tags.map((tag) => (
+                  <span
+                    key={tag.label}
+                    className={`px-2 py-1 rounded-full text-xs font-medium text-white ${tag.color}`}
+                  >
+                    {tag.label}
+                  </span>
+                ))}
+              </div>
+            </a>
           ))}
         </div>
       </motion.section>
