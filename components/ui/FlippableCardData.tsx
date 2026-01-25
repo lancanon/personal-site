a2bd2d8e2a09131f5c4dafea9e08464b5a69c432
audy <veeaudy1201@gmail.com>
2026-01-25 15:57:48 -0500

    update ui

--- components/ui/FlippableCardData.tsx
+++ components/ui/FlippableCardData.tsx
@@ -2,7 +2,7 @@ export const aboutImages = [
   '/assets/images/laos.JPG',
   '/assets/images/hike.JPG',
   '/assets/images/maple.png',
-  '/assets/images/rockclimb.jpg',
+  '/assets/images/niseko.jpg',
 ];
 
 export const cardAngles = ['-8deg', '-2deg', '2deg', '8deg'];
